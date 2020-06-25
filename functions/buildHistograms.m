35c3eea042fc3a186c3ebd669856f769919fefe7
kurganski <k-and92@mail.ru>
2020-06-26 01:08:23 +0500

    hist3 has been added

--- functions/buildHistograms.m
+++ functions/buildHistograms.m
@@ -1,4 +1,4 @@
-function buildHistograms(data, axesHandle, graphProperty)
+function buildHistograms(data, axesHandle, graphProperty, dataNames)
 
 if nargin == 1
     axesHandle = gca;
@@ -6,6 +6,14 @@ end
 
 if nargin == 2
     graphProperty = "Абсолютная";    
+end
+
+if graphProperty == "Трехмерная"    
+    hist3(axesHandle, data, 'CdataMode','auto','FaceColor','interp');
+    xlabel(dataNames(1,1));
+    ylabel(dataNames(2,1));
+    view([-45 30]);
+    return;
 end
 
 for x = 1:size(data,2)    
