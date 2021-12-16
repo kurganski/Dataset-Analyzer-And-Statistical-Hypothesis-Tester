0e1923735ccd0b7aa22f63dc656dd36991ce3beb
Andrew Kurganski <k-and92@mail.ru>
2021-12-17 01:32:50 +0500

    Add files via upload
    normalization of logit regression predictors is added

--- functions/calculateGeneralRegression.m
+++ functions/calculateGeneralRegression.m
@@ -2,7 +2,13 @@ function [infoTable, testTable] = calculateGeneralRegression( predictors, respon
                                                 distribution, linkFunction, varNames, trialsNum )
 
 if distribution == "binomial" 
-    mdl = fitglm(predictors, response, modelspec, 'BinomialSize', trialsNum, ...
+    
+    min_predictors = min(predictors);
+    max_predictors = max(predictors);
+    
+    normalizedPredictors = (predictors - min_predictors) ./ max_predictors;
+    
+    mdl = fitglm(normalizedPredictors, response, modelspec, 'BinomialSize', trialsNum, ...
                     'Distribution', distribution, 'Link', linkFunction);
 else
     mdl = fitglm(predictors, response, modelspec, 'Distribution', distribution, 'Link', linkFunction);    
@@ -16,8 +22,16 @@ end
 
 infoTable = table2cell(mdl.Coefficients);
 infoTable(:,end+1:end+2) = num2cell(coefCI(mdl));
-infoTable = cell2table(infoTable, 'VariableNames', [fieldnames(summary(mdl.Coefficients)); 'low_CI'; 'high_CI']', ...
+
+if ~isequal(modelspec,'constant')
+    infoTable(:,end+1:end+2) = num2cell(zeros(mdl.NumCoefficients,2));
+    infoTable(2:2+length(min_predictors)-1, end-1:end) = num2cell([min_predictors; max_predictors]');
+end
+
+infoTable = cell2table(infoTable, 'VariableNames', [fieldnames(summary(mdl.Coefficients)); ...
+                            'low_CI'; 'high_CI'; 'Min_norm_value'; 'Max_norm_value']', ...
                                     'RowNames', mdl.CoefficientNames);
+                                
 
 oldRowNames = string(infoTable.Properties.RowNames);
 newRowNames = setNewNames(oldRowNames, "x", varNames);
